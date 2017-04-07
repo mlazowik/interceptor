40e44e61d482be25c3f08907149d5f0e58885295
Michał Łazowik <mlazowik@gmail.com>
2017-04-07 11:07:46 +0200

    Swtich to a library project

---
+++ interceptor.h
@@ -0,0 +1,15 @@
+#ifndef INTERCEPTOR_H
+#define INTERCEPTOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *intercept_function(const char *name, void *new_func);
+void unintercept_function(const char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
